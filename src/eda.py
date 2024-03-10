46c3cbc748185b0aa950577c7a2ef07cb3fa0a86
Michael Massone <mikey@Michaels-MBP.lan>
2024-03-09 20:52:08 -0500

    Update2.2: mnor addition to eda.py

--- src/eda.py
+++ src/eda.py
@@ -22,17 +22,17 @@ def main():
 
     ## Visualizations
 
-
+    # Price distribution by fish type, boxplots
     price_distribution_boxplots(df)
-
+    # Price vs amount sold, by import/domestic and by fish type
     amnt_vs_price_scatterplots(df)
-
+    # Amount solde by species, total and by year
     amnt_sold_by_species_barplots(df)
-
+    # amount sold over time, 2004-2024
     amnt_sold_over_time_lineplot(df)
-
+    # amount sold over time, by species, domestic/imported
     amnt_sold_over_time_by_species_lineplots(df)
-
+    # amount sold over time by country of origin
     amnt_sold_over_time_by_origin_lineplots(df)
 
 if __name__ == "__main__":
