78eee34c2a4b4082e5bcb6e73c56633370fa2c25
Michael <massone.m@northeastern.edu>
2024-04-21 15:22:01 -0400

    signifigance figure update

--- src/significanceTesting_utils.py
+++ src/significanceTesting_utils.py
@@ -96,6 +96,7 @@ def significanceTester(modelDict):
     plt.figure(figsize=(10, 8))
     sns.heatmap(p_val_df, cmap="YlGnBu", annot=True,)
     plt.title("Model Significance Testing f_test P-Values Heatmap")
+    plt.xticks(rotation=45)
     plt.xlabel("Test Model")
     plt.ylabel("Null Model")
     plt.savefig('figs/model_significance_testing_p_vals.png', bbox_inches='tight')
